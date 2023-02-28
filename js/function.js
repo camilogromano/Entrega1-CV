f3521c38b367bf220afcd727d6dc411d8f4a5f39
camilogromano <camilogromano@gmail.com>
2023-02-28 20:11:00 -0300

    primera versión

---
+++ js/function.js
@@ -0,0 +1,27 @@
+
+
+/*function ocultar(){
+    document.getElementById('obj1').style.display = 'none';
+    }
+*/
+
+document.getElementById("usuario").onclick = function () {
+console.log("capturamos el evento click del botón usuario");
+document.getElementById("subtitulo").innerHTML = "Con Iniciativa y Dedicación";
+}
+
+document.getElementById("direccion").onclick = function () {
+console.log("capturamos el evento click del botón usuario");
+document.getElementById("subtitulo").innerHTML = "En la Ciudad de La PLata";
+}
+
+document.getElementById("titulo").onclick = function () {
+console.log("capturamos el evento click del botón usuario");
+document.getElementById("subtitulo").innerHTML = "Con experiencia en diferentes Niveles Educativos";
+}
+
+document.getElementById("trabajo").onclick = function () {
+console.log("capturamos el evento click del botón usuario");
+document.getElementById("subtitulo").innerHTML = "Cantante Profesional";
+}
+
